33e99d02b0be728ead97590a6e0a29b5ce35e113
davlatbek <idavlatbek19@gmail.com>
2018-01-31 01:26:17 +0300

    add methods to accountrepo interface

--- src/main/java/com/processingcenter/processingcenter/repositories/AccountRepository.java
+++ src/main/java/com/processingcenter/processingcenter/repositories/AccountRepository.java
@@ -1,7 +1,17 @@
 package com.processingcenter.processingcenter.repositories;
 
+import com.processingcenter.processingcenter.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
 /**
  * Created by davlet on 1/31/18.
+ * Интерфейс для доступа к объектам Account в бд.
  */
-public interface AccountRepository {
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    List<Account> findAll();
+    List<Account> findAllByLastNameStartsWithIgnoreCase(String lastName);
+    Account findByLastName(String lastName);
+    Account findBalanceByAccId(Long id);
 }
