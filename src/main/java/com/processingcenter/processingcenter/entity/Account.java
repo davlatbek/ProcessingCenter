3b3c23cd674cdcb98ae7720bd583a176a850fbaf
davlatbek <idavlatbek19@gmail.com>
2018-01-31 01:03:45 +0300

    implement account entity

--- src/main/java/com/processingcenter/processingcenter/entity/Account.java
+++ src/main/java/com/processingcenter/processingcenter/entity/Account.java
@@ -1,7 +1,72 @@
 package com.processingcenter.processingcenter.entity;
 
+import javax.persistence.*;
+
 /**
  * Created by davlet on 1/31/18.
+ * Таблица представляет собой объект банковский счет клиента (аккаунт)
  */
+
+@Entity
+@Table(name = "account")
 public class Account {
+
+    private Long accId;
+    private String firstName;
+    private String lastName;
+    private Integer balance;
+
+    protected Account() {
+    }
+
+    public Account(String firstName, String lastName, Integer balance) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    public Long getAccId() {
+        return accId;
+    }
+
+    public void setAccId(Long accId) {
+        this.accId = accId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Integer balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accId=" + accId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
 }
+
