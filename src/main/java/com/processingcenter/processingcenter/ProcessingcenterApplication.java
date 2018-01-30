701d8b4665eac32eeca920677e5759c4c72e2c6a
davlatbek <idavlatbek19@gmail.com>
2018-01-31 00:41:40 +0300

    upload clean project

---
+++ src/main/java/com/processingcenter/processingcenter/ProcessingcenterApplication.java
@@ -0,0 +1,12 @@
+package com.processingcenter.processingcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessingcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessingcenterApplication.class, args);
+	}
+}
