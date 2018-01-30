ff270daacf46c8ee5428457ad9628cc72c2cf7d2
davlatbek <idavlatbek19@gmail.com>
2018-01-31 01:28:50 +0300

    add method to transactinrepo interface

--- src/main/java/com/processingcenter/processingcenter/repositories/TransactionRepository.java
+++ src/main/java/com/processingcenter/processingcenter/repositories/TransactionRepository.java
@@ -1,7 +1,13 @@
 package com.processingcenter.processingcenter.repositories;
 
+import com.processingcenter.processingcenter.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
 /**
  * Created by davlet on 1/31/18.
  */
-public interface TransactionRepository {
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findAll();
 }
