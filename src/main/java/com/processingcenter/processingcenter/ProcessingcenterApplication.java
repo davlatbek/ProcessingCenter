bc0bc5300c6c32a05dc56acd0501551892089eed
davlatbek <idavlatbek19@gmail.com>
2018-01-31 19:01:46 +0300

    add some test objects to db

--- src/main/java/com/processingcenter/processingcenter/ProcessingcenterApplication.java
+++ src/main/java/com/processingcenter/processingcenter/ProcessingcenterApplication.java
@@ -1,12 +1,36 @@
 package com.processingcenter.processingcenter;
 
+import com.processingcenter.processingcenter.entity.Account;
+import com.processingcenter.processingcenter.entity.Transaction;
+import com.processingcenter.processingcenter.repositories.AccountRepository;
+import com.processingcenter.processingcenter.repositories.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
-public class ProcessingcenterApplication {
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class ProcessingcenterApplication implements CommandLineRunner{
+	@Autowired
+	AccountRepository accountRepository;
+	@Autowired
+	TransactionRepository transactionRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProcessingcenterApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		accountRepository.save(new Account("Tatyana", "Voteva", 31000));
+		accountRepository.save(new Account("Andrey", "Sokolov", 19000));
+		accountRepository.save(new Account("Gael", "Guedia", 150000));
+		accountRepository.save(new Account("Petr", "Petrov", 4000000));
+		transactionRepository.save(new Transaction(2L, 1L, 1000));
 	}
 }
