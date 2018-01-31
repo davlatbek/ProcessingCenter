ddd6d93b7a5baf3335a89bd62ce84a9cc4f2cb10
davlatbek <idavlatbek19@gmail.com>
2018-01-31 17:54:15 +0300

    change db name in url

--- src/main/java/com/processingcenter/processingcenter/config/AppConfiguration.java
+++ src/main/java/com/processingcenter/processingcenter/config/AppConfiguration.java
@@ -19,7 +19,7 @@ public class AppConfiguration {
     DataSource dataSource() {
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/pcdbtest");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/pcdb");
         dataSource.setUsername("postgres");
         dataSource.setPassword("1");
         return dataSource;
