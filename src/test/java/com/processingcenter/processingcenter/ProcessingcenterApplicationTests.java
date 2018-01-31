5c7dd8e68ca09ba98afdda3ae909f78ed32ab23a
davlatbek <idavlatbek19@gmail.com>
2018-01-31 20:16:04 +0300

    add testng to pom, add simple test

--- src/test/java/com/processingcenter/processingcenter/ProcessingcenterApplicationTests.java
+++ src/test/java/com/processingcenter/processingcenter/ProcessingcenterApplicationTests.java
@@ -1,16 +1,29 @@
 package com.processingcenter.processingcenter;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class ProcessingcenterApplicationTests {
+import com.processingcenter.processingcenter.config.AppConfiguration;
+import com.processingcenter.processingcenter.repositories.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@WebAppConfiguration
+@ComponentScan
+@ContextConfiguration(classes = AppConfiguration.class)
+public class ProcessingcenterApplicationTests extends AbstractTestNGSpringContextTests {
+
+	@Autowired
+	AccountRepository accountRepository;
 
 	@Test
-	public void contextLoads() {
+	public void test1() {
+		Assert.assertEquals(1, 1);
 	}
+
+
 
 }
