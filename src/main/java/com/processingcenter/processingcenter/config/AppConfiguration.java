634edf2aa7ba8a4aa466b863256ee74ee6bf7b95
davlatbek <idavlatbek19@gmail.com>
2018-02-01 15:52:18 +0300

    update app config class

--- src/main/java/com/processingcenter/processingcenter/config/AppConfiguration.java
+++ src/main/java/com/processingcenter/processingcenter/config/AppConfiguration.java
@@ -7,7 +7,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
@@ -18,20 +22,20 @@ import javax.sql.DataSource;
 @Configuration
 @EnableWebMvc
 @EnableJpaRepositories("com.processingcenter.processingcenter.repositories")
-public class AppConfiguration {
+public class AppConfiguration extends WebMvcConfigurerAdapter{
     @Bean
     DataSource dataSource() {
-//        BasicDataSource dataSource = new BasicDataSource();
-//        dataSource.setDriverClassName("org.postgresql.Driver");
-//        dataSource.setUrl("jdbc:postgresql://localhost:5432/pcdb");
-//        dataSource.setUsername("postgres");
-//        dataSource.setPassword("1");
-
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://ec2-23-23-92-179.compute-1.amazonaws.com:5432/d1ub4mtsogjkac?sslmode=require");
-        dataSource.setUsername("nanvkhyttqqhrx");
-        dataSource.setPassword("5c3b598b8dff2061d9a497e71294e5c572ef72203e14d8dc8d129da898ee88ac");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/pcdb");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("1");
+
+//        BasicDataSource dataSource = new BasicDataSource();
+//        dataSource.setDriverClassName("org.postgresql.Driver");
+//        dataSource.setUrl("jdbc:postgresql://ec2-23-23-92-179.compute-1.amazonaws.com:5432/d1ub4mtsogjkac?sslmode=require");
+//        dataSource.setUsername("nanvkhyttqqhrx");
+//        dataSource.setPassword("5c3b598b8dff2061d9a497e71294e5c572ef72203e14d8dc8d129da898ee88ac");
 
         return dataSource;
     }
@@ -50,5 +54,19 @@ public class AppConfiguration {
         factory.afterPropertiesSet();
 
         return factory.getObject();
+    }
+
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".html");
+        return resolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(
+            DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
     }
 }
